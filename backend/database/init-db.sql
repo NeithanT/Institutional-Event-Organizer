b28015bd9b72639ded555093b3ebf238795c6a9d
NeithanT <n.vargas.5@estudiantec.cr>
2026-03-10 16:16:54 -0600

    Base init and schema for DB

--- backend/database/init-db.sql
+++ backend/database/init-db.sql
@@ -1,16 +1,106 @@
-CREATE DATABASE Tarea01;
+CREATE DATABASE EventOrganizer;
 GO
 
-USE Tarea01;
+USE EventOrganizer;
 GO
 
-CREATE TABLE Usuario (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    Nombre varchar(64) NOT NULL,
-    Salario money NOT NULL
+-- Usuarios
+
+CREATE TABLE [User] (
+    Id          INT IDENTITY(1,1) PRIMARY KEY,
+
+    UserPas     VARCHAR(50) NOT NULL,
+    UserName        VARCHAR(100) NOT NULL,
+    Email       VARCHAR(64) NOT NULL,
+    Active      BIT NOT NULL,
+    RoleId      INT NOT NULL,
+    IdCard      INT
 );
+GO
 
-INSERT INTO Empleado (Nombre, Salario)
-VALUES ('Juan Perez', 200000.00)
+CREATE TABLE Role (
+    Id          INT IDENTITY(1,1) PRIMARY KEY,
 
+    RolName     VARCHAR(50) NOT NULL UNIQUE
+);
+GO
+
+-- EVENTOS
+
+CREATE TABLE Category (
+    Id          INT IDENTITY(1,1) PRIMARY KEY,
+
+    NameCategory    VARCHAR(50) NOT NULL UNIQUE
+);
+GO
+
+CREATE TABLE OrganizerEntity (
+    Id          INT IDENTITY(1,1) PRIMARY KEY,
+
+    EntityName  VARCHAR(50) NOT NULL UNIQUE,
+);
+GO
+
+CREATE TABLE Event (
+    Id                  INT IDENTITY(1,1) PRIMARY KEY,
+
+    EventDate           DATETIME NOT NULL,
+    Place               VARCHAR(100) NOT NULL,
+    Title               VARCHAR(100) NOT NULL,
+    EventDescription    VARCHAR(300) NOT NULL,
+    CategoryId          INT NOT NULL,
+    OrganizerId         INT NOT NULL,
+    OrganizerEntityId   INT NOT NULL,
+    AvalaibleEntries    INT NOT NULL,
+    ApprovedState       BIT NOT NULL
+);
+GO
+
+CREATE TABLE CanceledEvent (
+    Id      INT IDENTITY(1,1) PRIMARY KEY,
+
+    EventId INT NOT NULL,
+    Reason  VARCHAR(500) NOT NULL
+);
+GO
+
+CREATE TABLE Inscriptions (
+    Id      INT IDENTITY(1,1) PRIMARY KEY,
+
+    EventId INT NOT NULL,
+    UserId  INT NOT NULL
+);
+GO
+
+CREATE TABLE Attendance (
+    Id      INT IDENTITY(1,1) PRIMARY KEY,
+
+    EventId INT NOT NULL,
+    UserId  INT NOT NULL
+);
+GO
+
+-- Noticias
+
+CREATE TABLE New (
+    Id      INT IDENTITY(1,1) PRIMARY KEY,
+
+    WriterId    INT NOT NULL,
+
+    Title       VARCHAR(100) NOT NULL,
+    About       VARCHAR(100) NOT NULL,
+    Body        VARCHAR(1000) NOT NULL
+);
+GO
+
+CREATE TABLE Mail (
+    Id      INT IDENTITY(1,1) PRIMARY KEY,
+
+    WriterId    INT NOT NULL,
+    EventId     INT NOT NULL,
+    Title       VARCHAR(100) NOT NULL,
+    About       VARCHAR(100) NOT NULL,
+    Body        VARCHAR(1000) NOT NULL
+
+);
 GO
