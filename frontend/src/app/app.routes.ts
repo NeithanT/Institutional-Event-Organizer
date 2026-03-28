03ff59f769f48858ce5e2f95464d0ba66e08b85c
NeithanT <n.vargas.5@estudiantec.cr>
2026-03-27 21:25:57 -0600

    Added login page, router and basic design

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/login-page/login-page').then(m => m.LoginPage)
+  },
+
+];
+
