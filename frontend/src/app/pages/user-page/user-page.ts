6d96d250393e68a4fa7411bfb35374122a654262
MR.ERIK <80484346+ErikZF@users.noreply.github.com>
2026-03-30 17:20:07 -0600

    feat: add home page with navbar, footer, announcements and events

--- frontend/src/app/pages/user-page/user-page.ts
+++ frontend/src/app/pages/user-page/user-page.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-user-page',
-  imports: [],
+  standalone: true,
+  imports: [Navbar, Footer],
   templateUrl: './user-page.html',
-  styleUrl: './user-page.css',
+  styleUrl: './user-page.css'
 })
-export class UserPage {}
+export class UserPage {
+  anuncios = [
+    { titulo: 'Título: Cancelación del Evento Karaoke-Chan' },
+    { titulo: 'Título: Aumento de Cupos en Evento Soccer Match!' },
+  ];
+
+  eventos = [
+    { titulo: 'Evento 1', imagen: 'assets/evento1.jpg' },
+    { titulo: 'Evento 2', imagen: 'assets/evento2.jpg' },
+    { titulo: 'Evento 3', imagen: 'assets/evento3.jpg' },
+    { titulo: 'Evento 4', imagen: 'assets/evento4.jpg' },
+  ];
+}
