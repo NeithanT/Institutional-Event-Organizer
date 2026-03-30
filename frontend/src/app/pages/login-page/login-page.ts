3bb76f31163d6ed981aab435b6383f47811e5f51
NeithanT <n.vargas.5@estudiantec.cr>
2026-03-29 18:49:07 -0600

    Added User registration Screen

--- frontend/src/app/pages/login-page/login-page.ts
+++ frontend/src/app/pages/login-page/login-page.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { LoginLogo } from '../../components/login/login-logo/login-logo';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [LoginLogo],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
-export class LoginPage {}
+export class LoginPage {
+
+  isRegistering: boolean = false;
+
+  toggleRegister() {
+    this.isRegistering = true;
+  }
+
+  onLogin() {
+
+  }
+}
