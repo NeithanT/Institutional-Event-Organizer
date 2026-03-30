6a9010206eeaa2ba6adb38deba9d3809daf7c311
Sebastian Andres Chaves Rojas <s.chaves.4@estudiantec.cr>
2026-03-30 13:46:12 -0600

    Added connection string

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -1,10 +1,24 @@
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<EventOrganizerContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    }
+);
+
+
+
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
