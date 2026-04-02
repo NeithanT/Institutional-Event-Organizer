9414b8f13e5b0a34ee9ed653c92ad126905ef396
DayannaHercast <d.hernandez.7@estudiantec.cr>
2026-04-02 12:50:24 -0600

    fix: improve event form validation/comments

--- frontend/src/app/pages/organizer-page/create-event/create-event.ts
+++ frontend/src/app/pages/organizer-page/create-event/create-event.ts
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-create-event',
   standalone: true,
-  imports: [FormsModule, CommonModule], // 👈 AQUÍ
+  imports: [FormsModule, CommonModule], 
   templateUrl: './create-event.html',
   styleUrl: './create-event.css'
 })
@@ -21,23 +21,23 @@ export class CreateEvent implements OnInit {
   capacity: number = 0;
 
   categoryId: number = 0;
-  organizerId: number = 1; // ✅ fijo
+  organizerId: number = 1; 
   organizerEntityId: number = 0;
 
   selectedFile: File | null = null;
 
-  // 🔥 lista dinámica desde backend
+  // lista dinámica desde backend
   organizerEntities: any[] = [];
 
   constructor(private http: HttpClient) {}
 
-  // 🔥 se ejecuta al cargar la página
+
 ngOnInit() {
   console.log("INIT FUNCIONANDO");
   this.loadOrganizerEntities();
 }
 
-  // 🔥 traer unidades organizadoras desde backend
+  // traer unidades organizadoras desde backend
 loadOrganizerEntities() {
   this.http.get<any[]>('http://localhost:5053/organizer/get-entities')
     .subscribe({
@@ -51,7 +51,7 @@ loadOrganizerEntities() {
     });
 }
 
-  // 🔥 captura archivo real
+  //captura archivo real
   onFileSelected(event: any) {
     const file = event.target.files[0];
 
@@ -72,7 +72,7 @@ loadOrganizerEntities() {
 
     const formData = new FormData();
 
-    // 🔥 nombres EXACTOS del DTO (C#)
+   
     formData.append('Title', this.title);
     formData.append('EventDate', formattedDate);
     formData.append('Place', this.place);
@@ -83,7 +83,7 @@ loadOrganizerEntities() {
     formData.append('OrganizerId', this.organizerId.toString());
     formData.append('OrganizerEntityId', this.organizerEntityId.toString());
 
-    // 🔥 archivo real (IFormFile)
+  
     if (this.selectedFile) {
       formData.append('ImageFileEvent', this.selectedFile);
     }
